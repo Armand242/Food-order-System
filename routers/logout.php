3b7c8b4f13decbb908c5e7cad199a281b1b634aa
Armand <iae0444@gmail.com>
2020-07-11 19:53:38 +0800

    Initial

---
+++ routers/logout.php
@@ -0,0 +1,5 @@
+<?php
+    session_start();
+	session_destroy();
+	header("location: ../file:///C:/Users/ileaderx/Desktop/my%20files/Study%20Files/Team%20specification%20project%20training/food/w.html");
+?>
